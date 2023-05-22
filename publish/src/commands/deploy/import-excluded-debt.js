d6244125440b2b3c9eb5140bcbc761a4403cd744
rout-horizon <rout.c@horizonprotocol.com>
2023-05-23 03:32:10 +0530

    new testnet deployed

--- publish/src/commands/deploy/import-excluded-debt.js
+++ publish/src/commands/deploy/import-excluded-debt.js
@@ -7,7 +7,7 @@ module.exports = async ({ deployer, freshDeploy, runStep }) => {
 	console.log(gray(`\n------ IMPORT DEBT-CACHE EXCLUDED-DEBT RECORDS ------\n`));
 
 	const { DebtCache } = deployer.deployedContracts;
-
+	
 	// fresh deploys or no new debt cache mean this should be skipped
 	if (freshDeploy) {
 		console.log(gray(`freshDeploy - no excluded debt required to import. Skipping.`));
